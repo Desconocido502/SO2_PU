515d29aa615f6068ec2c4131f799c4351e932f3f
Carlos Soto <3018554470101@ingenieria.usac.edu.gt>
2024-06-24 16:44:43 -0600

    SO2-PU #comment UPDATE: Se modifico el backend #time 45m

--- backend/app.js
+++ backend/app.js
@@ -52,14 +52,52 @@ io.on('connection', (socket) => {
 
 async function emitLatestProcesses() {
   try {
-    const [rows] = await db.query('SELECT * FROM procesos ORDER BY request_time_date DESC LIMIT 600');
+    const [rows] = await db.query('SELECT * FROM procesos ORDER BY request_time_date DESC LIMIT 50');
     io.emit('latestProcesses', rows);
   } catch (error) {
     console.error('Error al emitir procesos:', error);
   }
 }
 
+async function emitGetTopMemoryProcesses() {
+  try {
+    // Obtener la memoria total
+    const [totalMemoryResult] = await db.query('SELECT SUM(segment_size) AS total_memory FROM procesos');
+    const totalMemory = totalMemoryResult[0].total_memory;
+
+    // Obtener los procesos con más memoria utilizada
+    const [topMemoryRows] = await db.query(`
+      SELECT
+        pid,
+        process_name,
+        (SUM(CASE WHEN call = 'mmap2' THEN segment_size ELSE 0 END) - SUM(CASE WHEN call = 'munmap' THEN segment_size ELSE 0 END)) AS memory
+      FROM procesos
+      GROUP BY pid, process_name
+      HAVING memory > 0
+      ORDER BY memory DESC
+      LIMIT 10
+    `);
+
+    // Calcular el porcentaje de memoria
+    const topProcesosConPorcentaje = topMemoryRows.map(proceso => {
+      const memoria = proceso.memory;
+      const porcentaje = totalMemory ? (memoria / totalMemory * 100).toFixed(2) : 0;
+      return {
+        pid: proceso.pid,
+        nombre: proceso.process_name,
+        memoria: memoria,
+        porcentaje: porcentaje
+      };
+    });
+
+    io.emit('topMemoryProcesses', topProcesosConPorcentaje);
+  } catch (error) {
+    console.error('Error al obtener procesos:', error);
+  }
+}
+
 setInterval(emitLatestProcesses, 5000); // Emitir cada 5 segundos
+setInterval(emitGetTopMemoryProcesses, 5000); // Emitir cada 5 segundos
 
 const PORT = process.env.PORT || 5000; // Asegúrate de que este puerto coincida con el que usas en el frontend
 
